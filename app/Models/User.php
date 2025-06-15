3b051d0bf0a27734bdd4cfbc2966a56c2c2e7f1f
amiceli <amiceli@letsignit.com>
2025-06-15 17:31:04 +0200

    fix: relation between user, tag and page

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,4 +50,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Page::class);
     }
+
+    public function tags()
+    {
+        return $this->hasMany(Tag::class);
+    }
 }
