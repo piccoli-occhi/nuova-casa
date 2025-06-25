4669703c42a882e7f48e2ed51c4b2054c8a9a2e0
amiceli <amiceli@letsignit.com>
2025-06-25 22:25:09 +0200

    chore: lint some code with biome

--- resources/js/types/ziggy.d.ts
+++ resources/js/types/ziggy.d.ts
@@ -1,11 +1,9 @@
-import { route } from 'ziggy-js';
-
 declare global {
-    let route: typeof route;
+    let route: typeof route
 }
 
-declare module '@vue/runtime-core' {
+declare module "@vue/runtime-core" {
     interface ComponentCustomProperties {
-        route: typeof route;
+        route: typeof route
     }
 }
