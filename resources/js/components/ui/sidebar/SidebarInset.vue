76d8ad2e9befcae25a5f64e5d97f865d5014e012
amiceli <amiceli@letsignit.com>
2025-07-14 10:58:44 +0200

    fix: some main colors

--- resources/js/components/ui/sidebar/SidebarInset.vue
+++ resources/js/components/ui/sidebar/SidebarInset.vue
@@ -1,21 +1,29 @@
+<template>
+    <main
+        data-slot="sidebar-inset"
+        :class="cn(
+            'main__content relative flex w-full flex-1 flex-col',
+            'md:peer-data-[variant=inset]:m-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow-sm md:peer-data-[variant=inset]:peer-data-[state=collapsed]:ml-0',
+            props.class,
+        )"
+    >
+        <slot />
+    </main>
+</template>
+
 <script setup lang="ts">
-import { cn } from "@/lib/utils"
-import type { HTMLAttributes } from "vue"
+import {cn} from "@/lib/utils"
+import type {HTMLAttributes} from "vue"
 
 const props = defineProps<{
     class?: HTMLAttributes["class"]
 }>()
 </script>
 
-<template>
-  <main
-    data-slot="sidebar-inset"
-    :class="cn(
-      'relative flex w-full flex-1 flex-col',
-      'md:peer-data-[variant=inset]:m-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow-sm md:peer-data-[variant=inset]:peer-data-[state=collapsed]:ml-0',
-      props.class,
-    )"
-  >
-    <slot />
-  </main>
-</template>
+<style scoped>
+    .main__content {
+        box-sizing: border-box;
+        padding-right: 20px;
+        padding-left: 20px;
+    }
+</style>
