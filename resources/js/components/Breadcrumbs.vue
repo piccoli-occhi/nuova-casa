6c286934bd76a18a3f9f832667258929af397924
amiceli <antoine@miceli.click>
2025-07-20 19:06:50 +0200

    chore: remove useless breadcrumb component

--- resources/js/components/Breadcrumbs.vue
+++ resources/js/components/Breadcrumbs.vue
@@ -1,7 +1,7 @@
 <template>
     <p-breadcrumb dark>
-        <p-breadcrumb-item link="/dashboard">
-            Home
+        <p-breadcrumb-item>
+            <p-icon icon="home"></p-icon>
         </p-breadcrumb-item>
         <p-breadcrumb-item
             v-for="(item, index) in breadcrumbs"
@@ -11,33 +11,9 @@
             {{ item.title }}
         </p-breadcrumb-item>
     </p-breadcrumb>
-    <!-- <Breadcrumb>
-        <BreadcrumbList>
-            <template v-for="(item, index) in breadcrumbs" :key="index">
-                <BreadcrumbItem>
-                    <template v-if="index === breadcrumbs.length - 1">
-                        <BreadcrumbPage>{{ item.title }}</BreadcrumbPage>
-                    </template>
-<template v-else>
-                        <BreadcrumbLink as-child>
-                            <Link :href="item.href ?? '#'">{{ item.title }}</Link>
-                        </BreadcrumbLink>
-                    </template>
-</BreadcrumbItem>
-<BreadcrumbSeparator v-if="index !== breadcrumbs.length - 1" />
-</template>
-</BreadcrumbList>
-</Breadcrumb> -->
 </template>
 
-<script
-    setup
-    lang="ts"
->
-// TODO Remove
-// import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from '@/components/ui/breadcrumb';
-import { Link } from "@inertiajs/vue3"
-
+<script setup lang="ts">
 interface BreadcrumbItemType {
     title: string
     href?: string
