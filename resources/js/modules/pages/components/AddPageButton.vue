8808d5b0ee0175b19a861b2f904cc3fe3df15da7
amiceli <antoine@miceli.click>
2025-07-20 19:06:49 +0200

    feat(pages): add components to edit, delete and create page

---
+++ resources/js/modules/pages/components/AddPageButton.vue
@@ -0,0 +1,154 @@
+<template>
+    <div>
+        <p-modal
+            ref="addPageModal"
+            dark
+        >
+            <span slot="title">
+                Add page - {{ props.tag.name }}
+            </span>
+            <span slot="text">
+                <div class="casa-add-page">
+                    <p-input-text
+                        label="URL"
+                        placeholder="Page link"
+                        required
+                        block
+                        v-model="newPageForm.url"
+                        :disabled="inProgress"
+                        :error="newPageForm.errors.url"
+                        @input="store.resetState()"
+                    ></p-input-text>
+                    <div v-if="graphDone && !inProgress">
+                        <hr />
+                        <p-leaf>
+                            <img
+                                class="casa-add-page__image"
+                                :src="newPageForm.icon"
+                                v-if="newPageForm.icon"
+                            />
+                            <img
+                                class="casa-add-page__image"
+                                :src="retroDefault"
+                                v-else
+                            />
+                        </p-leaf>
+                        <p-input-text
+                            label="title"
+                            block
+                            v-model="newPageForm.title"
+                            :error="newPageForm.errors.title"
+                        ></p-input-text>
+                    </div>
+                    <p-progress-bar
+                        striped
+                        :auto="200"
+                        v-if="inProgress"
+                        type="secondary"
+                    ></p-progress-bar>
+                </div>
+            </span>
+            <div class="casa-add-page__footer">
+                <p-button>
+                    cancel
+                </p-button>
+                <p-button
+                    type="secondary"
+                    @click="store.openGraph()"
+                    v-if="!graphDone"
+                    :disabled="inProgress"
+                >
+                    scrap
+                </p-button>
+                <p-button
+                    type="success"
+                    @click="store.savePage()"
+                    v-else
+                >
+                    save
+                </p-button>
+            </div>
+        </p-modal>
+        <p-button
+            dark
+            type="primary"
+            @click="openModal()"
+        >
+            cancel
+        </p-button>
+    </div>
+</template>
+
+<script
+    lang="ts"
+    setup
+>
+import { onMounted, ref, watch } from "vue"
+import { type Tag } from "@/modules/domain/Types"
+import retroDefault from "../../../../assets/404_retro.png"
+import { usePage } from "../stores/PageStore"
+
+const { store, inProgress, graphDone, newPageForm, status } = usePage()
+
+const addPageModal = ref<HTMLElement | null>(null)
+const props = defineProps<{
+    tag: Tag
+}>()
+
+onMounted(() => {
+    const overlay = document.querySelector("p-notification-handler")
+    store.setTage(props.tag.id)
+
+    watch(status, (e: string) => {
+        if (e === "failed") {
+            overlay?.pushNotification({
+                type: "danger",
+                canclose: true,
+                timeout: 4000,
+                text: "Something failed",
+            })
+        }
+        if (e === "success") {
+            overlay?.pushNotification({
+                type: "success",
+                canclose: true,
+                timeout: 4000,
+                text: "New page saved",
+            })
+            addPageModal.value.close()
+        }
+    })
+})
+
+function openModal() {
+    store.resetState()
+    addPageModal.value.open()
+}
+</script>
+
+<style scoped>
+    .casa-add-page {
+        width: 400px;
+
+        p-input-text {
+            display: block;
+            margin-bottom: 10px;
+        }
+
+        hr {
+            margin-top: 20px;
+            margin-bottom: 15px;
+        }
+    }
+
+    .casa-add-page__image {
+        height: 100px;
+    }
+
+    .casa-add-page__footer {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 10px;
+    }
+</style>
