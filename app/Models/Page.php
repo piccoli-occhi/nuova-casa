df4479d84b2e05ea3d566e2acb13358455a59a9e
amiceli <antoine@miceli.click>
2025-07-20 19:06:48 +0200

    fix: relation between user, tag and page

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -6,10 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Page extends Model
 {
-    protected $hidden = array("user_id");
+    protected $hidden = array("user_id", "tag", "tag_id");
+
+    protected $appends = ['parent'];
+
+    public function getParentAttribute()
+    {
+        return array(
+            'id' => $this->tag->id,
+            'name' => $this->tag->name,
+        );
+    }
 
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function tag()
+    {
+        return $this->belongsTo(Tag::class);
     }
 }
