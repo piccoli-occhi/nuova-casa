a14a87401de4bdb80e141f083cbb32eaf08e23c5
amiceli <antoine@miceli.click>
2025-07-20 19:06:49 +0200

    fix: update main routes and models

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -6,6 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
+    protected $hidden = array("user_id", "pages");
+
+    protected $appends = ['children'];
+
+    public function getChildrenAttribute()
+    {
+        return $this->pages->map(function ($map) {
+            return array(
+                "id" => $map->id,
+                "title" => $map->title,
+                "icon" => $map->icon,
+                "favorite" => $map->favorite,
+            );
+        });
+    }
+
     public function pages()
     {
         return $this->hasMany(Page::class);
